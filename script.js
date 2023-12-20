e13388a72a6249924979cf67fc6e1c70cc39078f
aubonee <chytahsin.2210@gmail.com>
2023-12-20 18:07:49 +0600

    calculate expense and budget done

---
+++ script.js
@@ -0,0 +1,121 @@
+const addExpenseButton = document.getElementById("add-expense-button");
+
+const descriptionInput = document.getElementById("description");
+const valueInput = document.getElementById("value");
+const selectInput = document.getElementById("type");
+
+const incomeList = document.getElementById("income-list");
+const expenseList = document.getElementById("expense-list");
+const totalIncome = document.getElementById("total-income");
+const totalExpense = document.getElementById("total-expense");
+const availableBudget = document.getElementById("available-budget");
+function formatMoney(value) {
+  return Math.abs(Number(value)).toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+  });
+}
+
+function calculateIncome() {
+  let sum = 0;
+  for (let item of incomeList.children) {
+    const valueString =
+      item.children[0].children[1].children[0].innerHTML.replace(/,/g, "");
+
+    // console.log(parseFloat(valueString));
+    sum += parseFloat(valueString);
+  }
+  totalIncome.innerHTML = formatMoney(sum);
+}
+calculateIncome();
+
+/**
+ * Task 1: Calculate total expense
+ */
+function calculateExpense() {
+  
+  let sum = 0;
+  for (let item of expenseList.children) {
+    const valueString =
+      item.children[0].children[1].children[0].innerHTML.replace(/,/g, "");
+
+    console.log(parseFloat(valueString));
+    sum += parseFloat(valueString);
+    
+  }
+  totalExpense.innerHTML = formatMoney(sum);
+  console.log(totalExpense.innerHTML)
+}
+calculateExpense();
+
+/**
+ * Task 2: Calculate the budget
+ */
+
+function calculateBudget() {
+  let budget = 0;
+  let income =  parseFloat(totalIncome.innerHTML.replace(/,/g, ""));
+  console.log(totalIncome.innerHTML);
+  let expense =  parseFloat(totalExpense.innerHTML.replace(/,/g, ""));
+  console.log(totalExpense.innerHTML);
+ budget = income-expense;
+ console.log(budget);
+ availableBudget.innerHTML = budget.toFixed(2);
+ console.log(availableBudget.innerHTML);
+}
+calculateBudget();
+
+/**
+ * Task 3: Delete Entry
+ */
+function deleteEntry() {}
+
+function addEntry() {
+  const type = selectInput.value;
+  const description = descriptionInput.value;
+  const value = valueInput.value;
+
+  // data validation
+  const errors = [];
+  if (description.length === 0) {
+    errors.push("Please enter the description");
+  }
+  if (value.length === 0) {
+    errors.push("Please enter the value");
+  }
+  if (errors.length > 0) {
+    alert(errors);
+    return;
+  }
+
+  // insert entry
+  const list = type === "income" ? incomeList : expenseList;
+  const sign = type === "income" ? "+" : "-";
+  const colorClass = type === "income" ? "text-green-600" : "text-red-600";
+
+  const newEntryHtml = `
+    <li class="py-2.5">
+      <div class="group flex justify-between gap-2 text-sm">
+        <span>${description}</span>
+        <div>
+          <span class="${colorClass}">${sign}${formatMoney(value)}</span>
+          <span
+            class="ml-2 hidden cursor-pointer font-medium text-red-500 group-hover:inline-block"
+          >
+            Delete
+          </span>
+        </div>
+      </div>
+    </li>
+    `;
+
+  // Approach 1:
+  list.innerHTML += newEntryHtml;
+
+  // update total income value
+  calculateIncome();
+ // update total expense value
+ calculateExpense();
+ calculateBudget();
+}
+
+addExpenseButton.addEventListener("click", addEntry);
